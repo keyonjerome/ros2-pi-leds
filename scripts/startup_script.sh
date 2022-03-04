81822471e8f4e771d19555f6cbc95014268483e5
Keyon-Jerome <keyonjerome@gmail.com>
2022-03-04 09:32:49 -0500

    update startup script directory

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-. /home/ubuntu/code/ros2-pi-leds/install/setup.bash
+. /home/ubuntu/code/ros2-pi-leds/ROS2CPP/led_strip_controller/install/setup.bash
 ros2 launch led_strip_controller led_launch.py
